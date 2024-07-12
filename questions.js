b9e65647ecfc506476e83672aad9cee53afcf7ec
Logan_Völkel <Logan_you_dont_know@Fuckoff.com>
2024-07-12 19:40:24 +0200

    intermediate_storage

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Für was steht HTML ?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Markup Language",
+            "B. Hyper Type Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Homepage Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Für was steht CSS ?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Computer Style Sheet",
+            "B. Cascading Style Sheet",
+            "C. Code Style Sheet",
+            "D. Computer Design Language"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Für was steht PHP ?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Für was Steht SQL ?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "Stylesheet Query Language",
+            "Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Für was steht XML ?",
+        answer: "B. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Extensible Markup Language",
+            "C. Explore Multible Language",
+            "D. Extra Markup Language"
+        ]
+    }
+]
